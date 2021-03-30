61a4e4344886c8ef50a4baa049bbf4f6d64918b6
ArunTeltia <teltia.arun18@gmail.com>
2021-03-30 18:23:06 +0530

    mascot added in the website

--- src/components/CardTab/CardTab.js
+++ src/components/CardTab/CardTab.js
@@ -6,22 +6,11 @@ import EqualizerIcon from "@material-ui/icons/Equalizer";
 import TelegramIcon from "@material-ui/icons/Telegram";
 import WebIcon from "@material-ui/icons/Web";
 import GetAppIcon from "@material-ui/icons/GetApp";
+import Mascot from "./export_3.png";
+
 const CardTab = () => {
   return (
     <div className="cardtab">
-      <div class="card_table_of_content">
-        <div className="card_list">
-          <h5 className="card_heading">Table of Contents</h5>
-          <div className="card_listitem">
-            <ArrowForwardIosIcon style={{ backgroundColor: "#1e1e1e" }} />{" "}
-            Introduction
-          </div>
-          <div className="card_listitem">
-            <ArrowForwardIosIcon style={{ backgroundColor: "#1e1e1e" }} />{" "}
-            Sakura Related Links
-          </div>
-        </div>
-      </div>
       <div class="card_table_of_content">
         <div className="card_list">
           <h5 className="card_heading">Talk with us</h5>
@@ -41,6 +30,16 @@ const CardTab = () => {
             <GitHubIcon style={{ backgroundColor: "#1e1e1e" }} /> Github
           </div>
         </div>
+      </div>
+      <div className="mascot">
+        <center>
+          <h4>Our Mascot</h4>
+          <img
+            src={Mascot}
+            style={{ width: "100%" }}
+            alt="project sakura mascot"
+          />
+        </center>
       </div>
     </div>
   );
