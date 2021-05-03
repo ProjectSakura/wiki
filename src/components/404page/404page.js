fb915bc5745c0afb945176776f9fa7f2a905b23f
Samriddhi <Agrawalsamriddhi83@gmail.com>
2021-05-03 17:28:52 +0530

    Updated embedded form
    * Updated embbed form
    
    * resolved 404 import css error

--- src/components/404page/404page.js
+++ src/components/404page/404page.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import "./404page.css";
+// import "./404page.css";
 import SettingEnv from "../Guides/SettingEnv/SettingEnv";
 import Image from "./404.gif";
 
 const NoMatchPage = () => {
-    return (
-      <div className="Page404">
+  return (
+    <div className="Page404">
       <div>
-      <img className="Image404" src={Image}></img>
+        <img className="Image404" src={Image}></img>
         {/* <h2>404...</h2>
         <h2>Page not found!</h2> */}
       </div>
-      </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default NoMatchPage;
-  
+export default NoMatchPage;
